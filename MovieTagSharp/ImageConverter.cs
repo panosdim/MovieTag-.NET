45669a35b762cbe72f342eb6ec7478498331eafd
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2020-06-10 14:51:20 +0300

    Add project files.

---
+++ MovieTagSharp/ImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MovieTagSharp
+{
+    class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string poster = (string)value;
+
+            return MainWindow.baseUrl + "w92" + poster;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
